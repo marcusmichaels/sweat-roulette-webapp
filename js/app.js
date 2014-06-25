0b85bf1f94c3db13877c2652e3b70cd30e298568
Marcus Michaels <marcusmmichaels@gmail.com>
2014-06-25 15:54:32 +0100

    Big updates!
    - Added 20 second rest period between exercises
    - Added ‘Round’ counter
    - Added ability to share prewritten message about completed rounds.
    
    - and fixed / tweaked some other bits too.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 
 var exercises,
+    count,
     workout,
     timer,
+    restTimer,
     rand,
     seconds,
     farewell,
     farewell_message,
+    totalrounds,
     dothis = document.getElementById('sweat_roulette'),
     button = document.getElementById('button'),
     audioStart = document.getElementById('audio_start'),
     audioNext = document.getElementById('audio_next'),
     audioStop = document.getElementById('audio_stop');
+
+var tweetStart = '<a class="tweet" href="http://twitter.com/intent/tweet?text=I%20lasted%20';
+var tweetEnd = '%20rounds%20on%20%23sweatroulette%20-%20http%3A%2F%2Fsweatroulette.com" target="_blank"> - Tweet your score - </a>';
+
 
 function generateExercise() {
 
@@ -21,7 +28,9 @@ function generateExercise() {
     'Burpees',
     'High Knees',
     'Stretch',
-    'Star Jumps'
+    'Star Jumps',
+    'Side Plank',
+    'Plank'
   ];
 
   workout = exercises[Math.floor(Math.random() * exercises.length)];
@@ -34,21 +43,45 @@ function generateExercise() {
 }
 
 function startRoutine(){
+  count = 0;
+  totalrounds = 0;
 
   button.innerHTML = '<a src="javascript:void(0);" onclick="stopRoutine();" class="exercise stop" >I\'m Done.</a>';
 
   generateExercise();
 
   function loop() {
-      rand = Math.round(Math.random() * (60000 - 20000)) + 20000;
-      seconds = Math.round(rand / 1000 % 60);
+    rand = Math.round(Math.random() * (60000 - 20000)) + 20000;
+    seconds = Math.round(rand / 1000 % 60);
 
-      timer = setTimeout(function() {
-              
-        generateExercise();
+    timer = setTimeout(function() {
+      
+      count++;
+      generateExercise();
+
+      if(count % 2 === 0){
+
         loop();
 
-      }, rand);
+      } else {
+
+        totalrounds++;
+        console.log('Round ' + totalrounds);
+        restLoop();
+
+      }
+
+    }, rand);
+  }
+
+  function restLoop() { 
+
+    dothis.innerHTML = '<h2 class="workout">Rest</h2>';
+
+    restTimer = setTimeout(
+      function(){
+        loop();
+      }, 20000);
   }
 
   loop();
@@ -69,12 +102,17 @@ function stopRoutine() {
   farewell = farewell_message[Math.floor(Math.random() * farewell_message.length)];
 
     clearTimeout(timer);
-    dothis.innerHTML = '<h2 class="farewell">' + farewell + '</h2>';
+    clearTimeout(restTimer);
+
+    dothis.innerHTML = '<h2 class="farewell">' + farewell + '</h2><div class="rounds"> <p>You lasted ' + totalrounds + ' rounds<br>' + tweetStart + totalrounds + tweetEnd + '</p></div>';
 
   
   audioStop.pause();
   audioStop.play();
+
 }
+
+
 
 // function playMusic() {
 //   if(!audioMusic.paused){
